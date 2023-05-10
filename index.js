37aad50f4a0711f739ec76861cf4b5ab465839b3
mbron770 <mbronfin.tsd@gmail.com>
2023-05-10 13:07:31 -0400

    add event listener

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+
+
+//const input = document.getElementById('button');
+
 function addingEventListener() {
-}
+    const input = document.getElementById('button');
+    input.addEventListener('click', addingEventListener);
+    console.log(alert('I was clicked!'));
+    
+
+} 
+
+
+//input.addEventListener('click', addingEventListener);
